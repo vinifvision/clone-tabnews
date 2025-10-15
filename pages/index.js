72a996151e02d9d1017efe9954791f32c9a9665f
Viniflr <limarezendev1@gmail.com>
2025-10-15 17:08:09 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>"Não sei, só sei que foi assim" Essa fala é do filme O Auto da Compadecida, se você já assistiu esse fime, faz um sinal de joinha pra mim, caso contrário, crie uma lista de filmes que você precisa assistir urgentemente e coloca esse no topo dela!</h1>
+}
+
+export default Home;
